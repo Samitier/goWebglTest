be2b21f99320a6daf4a02fd459b6ec16281abe87
Blai Samitier <blai@samitier.me>
2016-12-04 14:11:20 +0100

    rotate button

--- client/app/pages/isometric-map/isometric-map.vue
+++ client/app/pages/isometric-map/isometric-map.vue
@@ -1,4 +1,9 @@
 <template>
+    <div class='action-buttons-container'>
+        <button class='rotate' @click="toggleRotate">{{ isRotating ? 'Stop' : 'Rotate' }}</button>
+        <button class="edit">Edit</button>
+        <button class="reset">Reset</button>
+    </div>
 </template>
 
 <script>
@@ -8,11 +13,41 @@
         components: {
         },
         created: function() {
-            let isometricMap = new IsometricMap()
+            this.isometricMap = new IsometricMap()
+        },
+        data: () => ({
+            isometricMap: {},
+            isRotating: false
+        }),
+        methods: {
+            toggleRotate: function() {
+                this.isRotating = !this.isRotating
+                this.isometricMap.setRotateAnimation(this.isRotating)
+            }
         }
     }
 </script>
 
 <style scoped>
-
+    .action-buttons-container {
+        position:absolute;
+        z-index:10;
+        bottom: 100px;
+        left:50%;
+        margin-left:-100px;
+        width:200px;
+        text-align:center;
+    }
+    .action-buttons-container > button {
+        border: 2px solid white;
+        background-color: transparent;
+        color: white;
+        padding: 0.5em 1em;
+        cursor: pointer;
+        transition: all 0.5s;
+    }
+    .action-buttons-container > button:hover {
+        background-color: white;
+        color: #444444;
+    }
 </style>
--- client/app/pages/isometric-map/isometric-map.webgl.js
+++ client/app/pages/isometric-map/isometric-map.webgl.js
@@ -1,7 +1,11 @@
 import { OrthographicCamera, DirectionalLight, BoxGeometry, MeshLambertMaterial, Mesh } from "three"
 import { ThreeScene } from "../../webgl-utils/three-scene.js"
  
-const CUBE_SIZE = 20
+const   CUBE_SIZE = 24,
+        CAMERA_X = 200,
+        CAMERA_Y = 150,
+        CAMERA_Z = 200,
+        TIME_STEP = 0.01
 
 export class IsometricMap extends ThreeScene {
     
@@ -11,15 +15,15 @@ export class IsometricMap extends ThreeScene {
     }
 
     init() {
+        //Camera setup
         let xSize = window.innerWidth / 2, 
             ySize  = window.innerHeight / 2
         this.camera = new OrthographicCamera( -xSize, xSize, ySize, -ySize, - 500, 1000 )
-        this.camera.position.set(200,100,200)
 
+        //Painting map
 		let geometry = new BoxGeometry( CUBE_SIZE, CUBE_SIZE, CUBE_SIZE ),
             material = new MeshLambertMaterial( { color: 0xffffff } ),
             map = this.getMap()
-
         for( let i = 0; i < map.length; ++i ) {
             for ( let j = 0; j < map[i].length; ++j ) {
                 let height = map[i][j]
@@ -35,19 +39,33 @@ export class IsometricMap extends ThreeScene {
             }
         }
 
+        //Light setup
         let directionalLight = new DirectionalLight( 0xffffff, 1 )
         directionalLight.position.set(1, 1, 0)
         this.scene.add( directionalLight )
 
+        //Animation setup
+        this.time = 0.8
+        this.camera.position.set(
+            Math.cos( this.time ) * CAMERA_X, 
+            CAMERA_Y, 
+            Math.sin( this.time ) * CAMERA_Z
+        )
+        this.camera.lookAt( this.scene.position )
+        this.rotateAnimation = false
+
+        //Render setup
         this.renderer.setClearColor( 0x92ccdd )
         super.render()
     }
  
     animate() {
-        let timer = Date.now() * 0.0001
-		this.camera.position.x = Math.cos( timer ) * 200
-		this.camera.position.z = Math.sin( timer ) * 200
-        this.camera.lookAt( this.scene.position )
+        if(this.rotateAnimation) {
+            this.time += TIME_STEP
+            this.camera.position.x = Math.cos( this.time ) * CAMERA_X
+            this.camera.position.z = Math.sin( this.time ) * CAMERA_Z
+            this.camera.lookAt( this.scene.position )
+        }
     }
 
     onResizeWindow() {
@@ -61,13 +79,26 @@ export class IsometricMap extends ThreeScene {
         super.resize()
     }
 
+    setRotateAnimation(isRotating) {
+        this.rotateAnimation = isRotating
+    }
+
     getMap() {
         return [
-            [0,0,1,0,0],
-            [0,1,2,1,0],
-            [1,2,3,2,1],
-            [0,1,2,1,0],
-            [0,0,1,0,0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0],
+            [1.0, 1.0, 1.5, 2.0, 1.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.5, 1.5, 2.0, 2.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [1.0, 1.5, 2.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [0.0, 0.0, 2.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+            [0.0, 1.5, 1.5, 1.5, 1.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0],
+            [1.0, 1.5, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0],
+            [1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0],
         ]
     }
 }
