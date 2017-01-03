ec64978e16b3690f5d23225b046e8348a62707c9
Blai Samitier <blai@samitier.me>
2017-01-03 15:06:49 +0100

    vue router in front-end

--- client/main.js
+++ client/main.js
@@ -1,24 +1,18 @@
 require('./styles/index.css')
 import Vue from 'vue'
 import routes from './routes'
+import VueRouter from 'vue-router'
+import app from './app.vue'
 
-const app = new Vue({
+Vue.use(VueRouter)
+
+const router = new VueRouter({ 
+    mode: 'history',
+    routes
+})
+
+new Vue({ 
     el: '#app',
-    data: {
-        currentRoute: window.location.pathname
-    },
-    computed: {
-        ViewComponent() {
-            const matchingView = routes[this.currentRoute] 
-            return require(`./pages/${ matchingView || 'errors' }/${ matchingView || '404' }.vue`)
-        }
-    },
-    render(h) {
-        return h(this.ViewComponent)
-    }
+    router,
+    render: h => h(app) 
 })
-
-window.addEventListener('popstate', () => {
-    app.currentRoute = window.location.pathname
-})
-
--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,13 @@
-export default {
-    '/': 'home',
-    '/about': 'about',
-    "/isometric-map": "isometric-map",
-    "/shaders" : "shaders"
-}
+import Home from './pages/home/home.vue'
+import About from './pages/about/about.vue'
+import IsometricMap from './pages/isometric-map/isometric-map.vue'
+import Shaders from './pages/shaders/shaders.vue'
+import NotFound from './pages/errors/404.vue'
+
+export default [
+    { path: '/', component: Home, name: 'home' },
+    { path:'/login', component: About, name: 'login' },
+    { path:'/isometric-map', component: IsometricMap, name: 'isometricMap' },
+    { path:'/shaders', component: Shaders, name: 'shaders' },
+    { path: '*', component: NotFound, name: 'notFound' } 
+]
