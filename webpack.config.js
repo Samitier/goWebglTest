56825c30737c3adac620ac14564565777348e12a
Blai Samitier <blai@samitier.me>
2017-02-19 19:28:47 +0100

    update to webpack 2x

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,91 @@
-let path = require('path')
-let webpack = require('webpack')
-let ExtractTextPlugin = require("extract-text-webpack-plugin")
-let HtmlWebpackPlugin = require('html-webpack-plugin')
-let release = process.argv.indexOf('--release') !== -1
+module.exports = function(env) {
 
+    let path = require('path'),
+        webpack = require('webpack'),
+        ExtractTextPlugin = require("extract-text-webpack-plugin"),
+        HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-    entry: {
-        app: './client/app-frontend/main.js',
-        backend: "./client/app-backend/main.js"
-    },
-    output: {
-        path: path.resolve(__dirname, './public'),
-        publicPath: '/',
-        filename: 'assets/[name].bundle.js'
-    },
-    resolveLoader: {
-        root: path.join(__dirname, 'node_modules')
-    },
-    resolve: {
-        root: [ path.resolve('./client') ],
-        extensions: ['', '.js', '.vue']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({ 
-            template: './client/app-frontend/index.html', 
-            hash: true, 
-            chunks: ['app'],
-            filename: 'index.html' 
-        }),
-        new HtmlWebpackPlugin({ 
-            template: './client/app-backend/index.html', 
-            hash: true, 
-            chunks: ['backend'],
-            filename: 'backend.html' 
-        }),
-        new ExtractTextPlugin("assets/style.bundle.css", { allChunks: true })
-    ],
-    module: {
-        loaders: [
-            {
-                test: /\.vue$/,
-                loader: 'vue',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader?minimize"),
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'file',
-                query: {
-                    name: '[name].[ext]?[hash]'
+    let config = {
+        entry: {
+            app: './client/app-frontend/main.js',
+            backend: "./client/app-backend/main.js"
+        },
+        output: {
+            path: path.resolve(__dirname, './public'),
+            publicPath: '/',
+            filename: 'assets/[name].bundle.js'
+        },
+        resolve: {
+            modules: [ 
+                path.resolve('./client'),
+                "node_modules"
+            ],
+            extensions: ['.js', '.vue', '.css']
+        },
+        plugins: [
+            new HtmlWebpackPlugin({ 
+                template: './client/app-frontend/index.html', 
+                hash: true, 
+                chunks: ['app'],
+                filename: 'index.html' 
+            }),
+            new HtmlWebpackPlugin({ 
+                template: './client/app-backend/index.html', 
+                hash: true, 
+                chunks: ['backend'],
+                filename: 'backend.html' 
+            }),
+            new ExtractTextPlugin({ filename: "assets/style.bundle.css", allChunks: true })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader' }),
+                },
+                {
+                    test: /\.(png|jpg|gif|svg)$/,
+                    loader: 'file',
+                    query: {
+                        name: '[name].[ext]?[hash]'
+                    }
                 }
-            }
-        ]
-    },
-    devtool: '#eval-source-map'
-}
+            ]
+        },
+        devtool: '#eval-source-map',
+        devServer: {
+           proxy: {
+               "**": "http://localhost:3001"
+           }
+        }
+    }
 
-if (process.env.NODE_ENV === 'production' || release) {
-    module.exports.devtool = ''
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.NoErrorsPlugin(),
-        new webpack.optimize.DedupePlugin()
-    ])
+    if (process.env.NODE_ENV === 'production' || env && env.release) {
+        config.devtool = ''
+        config.plugins = (config.plugins || []).concat([
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: '"production"'
+                }
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            }),
+            new webpack.NoEmitOnErrorsPlugin(),
+        ])
+    }
+
+    return config
 }
